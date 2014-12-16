6d92454cf05ed7920234c3b885193f7894a984be
Jake Worth <jakeworth82@gmail.com>
2014-12-16 11:37:28 -0600

    Add tests limiting input to legitimate values

--- lib/secret_santa.rb
+++ lib/secret_santa.rb
@@ -1,5 +1,13 @@
 class SecretSanta
-  def self.hi
-    puts "Secret Santa"
+  def self.arrayify_input(*args)
+    [*args]
+  end
+
+  def self.create_list(array)
+    if array.length <= 2
+      "List too short"
+    elsif array.length != array.uniq.length
+      "Please enter unique names"
+    end
   end
 end
