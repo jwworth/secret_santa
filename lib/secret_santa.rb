464cf942356cd656a898dcc0d89e41459874451d
Jake Worth <jakeworth82@gmail.com>
2014-12-17 10:24:30 -0600

    Return santa-giftee list

--- lib/secret_santa.rb
+++ lib/secret_santa.rb
@@ -8,6 +8,12 @@ class SecretSanta
       "List too short"
     elsif names.length != names.uniq.length
       "Please enter unique names"
+    else
+      # shuffle names and create hash (currently untested)
+      shuffled_names = names.dup.shuffle!
+      shuffled_names.each_with_index do |name, i|
+        puts "#{name} -> #{shuffled_names[i - 1]}"
+      end
     end
   end
 end
