96c9a337e340da403470c1db82463766d85dea80
Jake Worth <jakeworth82@gmail.com>
2014-12-19 15:11:50 -0600

    Prefer single quoted strings

--- lib/secret_santa.rb
+++ lib/secret_santa.rb
@@ -2,9 +2,9 @@ class SecretSanta
   # Create the Secret Santa list
   def self.create_list(names)
     if names.length <= 2
-      "ERROR: List too short"
+      'ERROR: List too short'
     elsif names.length != names.uniq.length
-      "ERROR: Please enter unique names"
+      'ERROR: Please enter unique names'
     else
       # Build the list
       list = []
@@ -12,7 +12,7 @@ class SecretSanta
       names.shuffle!
       names.each_with_index { |name, i| list << "#{name} -> #{names[i - 1]}" }
       # Write the list to a graphviz dot file
-      digraph_list = list.join("; ")
+      digraph_list = list.join('; ')
       digraph = "digraph {#{digraph_list}}\n"
       File.open("#{Time.now.year}_secret_santa_list.dot", 'w') { |f| f.write("#{digraph}") }
       # Return the list
@@ -24,8 +24,8 @@ class SecretSanta
 
   # Get Secret Santa names and handle
   def self.solicit_input
-    puts "Enter each name in the Secret Santa pool, separated by a space:"
+    puts 'Enter each name in the Secret Santa pool, separated by a space:'
     names = gets.downcase.chomp
-    create_list(names.split(" "))
+    create_list(names.split(' '))
   end
 end
